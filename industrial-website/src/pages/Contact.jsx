f58f4ef0bc1225411bb171655f789968d5506b75
Nikhil Shinde <nikhil892003.93.20@gmail.com>
2026-06-03 15:37:49 +0530

    pages/contact.js

--- industrial-website/src/pages/Contact.jsx
+++ industrial-website/src/pages/Contact.jsx
@@ -40,7 +40,7 @@ export default function Contact() {
 
   try {
     const response = await fetch(
-      "http://localhost:5000/api/contact",
+      "/api/contact",
       {
         method: "POST",
         headers: {
